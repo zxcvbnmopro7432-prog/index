16f09574becb8f5b6a94c98183fc3e4edc702291
zxcvbnmopro7432-prog <zxcvbnmopro7432@gmail.com>
2026-05-22 11:20:35 +0530

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,3 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  onSnapshot,
+  updateDoc,
+  doc,
+  serverTimestamp,
+  query,
+  orderBy
+} from "https://www.gstatic.com/firebasejs/12.13.0/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCIeG0RPxsD09y1lCyJZ7cx46YF-aMfCdQ",
   authDomain: "cold-corner-f3244.firebaseapp.com",
@@ -5,4 +19,20 @@ const firebaseConfig = {
   storageBucket: "cold-corner-f3244.firebasestorage.app",
   messagingSenderId: "574603200220",
   appId: "1:574603200220:web:4e9492e8b5b609d3b49619"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export {
+  db,
+  collection,
+  addDoc,
+  onSnapshot,
+  updateDoc,
+  doc,
+  serverTimestamp,
+  query,
+  orderBy
 };
